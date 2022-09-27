84c3a171d2712428e3e0af4ba3d7b8db26276e64
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2022-09-27 23:52:11 +1000

    Begin work on transformer

--- bible/parser.py
+++ bible/parser.py
@@ -1,15 +1,57 @@
 from pathlib import Path
 
-from lark import Lark
+from lark import Lark, Transformer
+
 
 GRAMMAR_FILE_NAME = "bible-reference.lark"
+
+
+class BibleRefTransformer(Transformer):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cur_book_name = None
+        self.cur_chap_num = 0
+    
+    def book_only_ref(self, children):
+        book = children[0]
+        start_chap = 1
+        start_verse = 1
+        end_chap = 9999
+        end_verse = 9999
+        self.cur_book_name = book
+        self.cur_chap_num = end_chap
+        return (book, start_chap, start_verse, end_chap, end_verse)
+
+    def book_num_ref(self, children):
+        book = children[0]
+        start_chap = children[1]
+        start_verse = 1
+        end_chap = start_chap
+        end_verse = 9999
+        self.cur_book_name = book
+        self.cur_chap_num = end_chap
+        return (book, start_chap, start_verse, end_chap, end_verse)
+
+    def BOOK_NAME(self, token):
+        return str(token)
+
+    def CHAP_NUM(self, token):
+        return int(token)
+
+    def VERSE_NUM(self, token):
+        return int(token)
+
+    def NUM(self, token):
+        return int(token)
 
 def parse():
     grammar_path = Path(__file__, "..", GRAMMAR_FILE_NAME).resolve()
     with open(grammar_path) as file:
                 grammar_text = file.read()
     parser = Lark(grammar_text)
-    tree = parser.parse("John 3:16-18; Romans 1:10-22; 3:20-22, 24")
+    tree = parser.parse("Matthew; Mark 2; John 3:16-18; Romans 1:10-22; 3:20-22, 24")
+    tree = BibleRefTransformer().transform(tree)
     print(tree)
     print(tree.pretty())
 
