706622878a13976919aba5bf0062e76643122a8b
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2022-10-28 12:21:51 +1000

    Extra parser test passage

--- test/test_parser.py
+++ test/test_parser.py
@@ -7,7 +7,7 @@ from bible.parser import BibleRefParsingError, _parse as parse
 class TestBibleParser(unittest.TestCase):
     def test_parse(self):
         try:
-            range_list = parse("Matthew; Mark 2; Jude 5; 8; Obadiah 2-3; John 3.16-18; " + 
+            range_list = parse("Matthew; Mark 2; Jude 5; 8; Obadiah 2-3; John 3.16-18; 10-14:2;" + 
                             "Romans 1:10-22; 2; 3:20-22, 24, 4:2-5:2, 10")
         except BibleRefParsingError as e:
             self.fail(str([str(e), e.start_pos, e.end_pos]))
@@ -21,6 +21,7 @@ class TestBibleParser(unittest.TestCase):
             [BibleRange(BibleBook.Jude, 1, 8)],
             [BibleRange(BibleBook.Obad, 1, 2, None, None, 3)],
             [BibleRange(BibleBook.John, 3, 16, None, None, 18)],
+            [BibleRange(BibleBook.John, 10, 1, None, 14, 2)],
             [BibleRange(BibleBook.Rom, 1, 10, None, None, 22)],                      
             [BibleRange(BibleBook.Rom, 2)],
             [
@@ -30,6 +31,6 @@ class TestBibleParser(unittest.TestCase):
                 BibleRange(BibleBook.Rom, 5, 10),
             ]
         ]
-        # pprint(range_list.to_nested_lists())
+        pprint(range_list.to_nested_lists())
         # pprint(expected_list)
         self.assertEqual(range_list.to_nested_lists(), expected_list)
