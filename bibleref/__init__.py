1b977ae7da2734ec335d69f433fbd030d58a42d1
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2023-01-17 01:03:50 +1000

    Remove duplicated documentation

--- bibleref/__init__.py
+++ bibleref/__init__.py
@@ -51,21 +51,7 @@ The global attribute `bibleref.ref.flags` is a `bibleref.ref.BibleFlag` enum who
 behaviour. Many methods take a `flags` keyword-argument that overrides the global `flags` attribute during the
 execution of that method.
 
-The effect of each flag is as follows:
-
-### `bibleref.ref.BibleFlag.MULTIBOOK`
-
-Defaults to unset. When set, `BibleRange`s can be constructed that span multiple books. Existing multibook ranges
-behave correctly even when `MULTIBOOK` is unset.
-
-### `bibleref.ref.BibleFlag.VERSE_0`
-
-Defaults to unset. When set, BibleVerses can be constructed where the first verse number of some chapters is `0`,
-not `1`. (This is currently just the Psalms that have superscriptions.) When you need to mix references that do or
-don't allow for verse 0, it may be easier to choose one value for all your code, and then use the `verse_0_to_1()`
-and `verse_1_to_0()` methods on `BibleVerse`, `BibleRange` and `BibleRangeList` as necessary.
-
-
+# Top-Level Classes
 '''
 class BibleRefException(Exception):
     '''Parent class for all Exception types in this package.'''
