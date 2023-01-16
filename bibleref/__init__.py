86b1d921eadd39d5bcc38b6b8777b8cd0d2c435f
jamesbcd <50201699+jamesbcd@users.noreply.github.com>
2023-01-16 21:07:16 +1000

    Improve global flags documentation

--- bibleref/__init__.py
+++ bibleref/__init__.py
@@ -22,9 +22,9 @@ The Bible book, chapter and verse data is specified in the `bibleref.data` sub-m
 
 # Global Flags
  
-The global attribute `bibleref.ref.flags` is a `bibleref.ref.BibleFlag` enum whose elements control
-package-wide behaviour. Many methods take a `flags` keyword-argument that overrides the global `flags` attribute
-during the execution of that method.
+The global attribute `bibleref.ref.flags` is a `bibleref.ref.BibleFlag` enum whose elements control package-wide
+behaviour. Many methods take a `flags` keyword-argument that overrides the global `flags` attribute during the
+execution of that method.
 
 The effect of each flag is as follows:
 
@@ -35,10 +35,10 @@ behave correctly even when `MULTIBOOK` is unset.
 
 ### `bibleref.ref.BibleFlag.VERSE_0`
 
-Defaults to unset. When set, BibleVerses can be constructed where the first verse number of some chapters is 0, not
-1. (This is currently just the Psalms that have superscriptions.) When you need to mix references that do or don't
-allow for verse 0, it may be easier to choose one value for all your code, and then use the `verse_0_to_1()` and
-`verse_1_to_0()` methods on `BibleVerse`s, `BibleRange`s and `BibleRangeList`s as necessary.
+Defaults to unset. When set, BibleVerses can be constructed where the first verse number of some chapters is `0`,
+not `1`. (This is currently just the Psalms that have superscriptions.) When you need to mix references that do or
+don't allow for verse 0, it may be easier to choose one value for all your code, and then use the `verse_0_to_1()`
+and `verse_1_to_0()` methods on `BibleVerse`, `BibleRange` and `BibleRangeList` as necessary.
 
 
 '''
